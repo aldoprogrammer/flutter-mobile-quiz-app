55a84143a5be79389b769e922ad40008780897d8
aldoprogrammer <aldobesma@gmail.com>
2024-09-15 13:26:15 +0700

    add some component

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,69 +11,121 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-          padding: EdgeInsets.all(8),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 421,
-                width: 400,
-                child: Stack(
-                  children: [
-                    Container(
-                      height: 240,
-                      width: 390,
-                      decoration: BoxDecoration(
-                        color: const Color(0xffA42FC1),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 421,
+              width: 400,
+              child: Stack(
+                children: [
+                  Container(
+                    height: 240,
+                    width: 390,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffA42FC1),
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                    Positioned(
-                      bottom: 60,
-                      left: 22,
-                      child: Container(
-                        height: 179,
-                        width: 350,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20),
-                            boxShadow: [
-                              BoxShadow(
-                                  offset: Offset(0, 1),
-                                  blurRadius: 5,
-                                  spreadRadius: 3,
-                                  color:
-                                      const Color(0xffA42FC1).withOpacity(.4))
-                            ]),
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 18),
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    '05',
-                                    style: TextStyle(
-                                        color: Colors.green, fontSize: 20),
-                                  ),
-                                  Text(
-                                    '07',
-                                    style: TextStyle(
-                                        color: Colors.red, fontSize: 20),
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
+                  ),
+                  Positioned(
+                    bottom: 60,
+                    left: 22,
+                    child: Container(
+                      height: 179,
+                      width: 350,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                                offset: const Offset(0, 1),
+                                blurRadius: 5,
+                                spreadRadius: 3,
+                                color: const Color(0xffA42FC1).withOpacity(.4))
+                          ]),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 18),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  '05',
+                                  style: TextStyle(
+                                      color: Colors.green, fontSize: 20),
+                                ),
+                                Text(
+                                  '07',
+                                  style: TextStyle(
+                                      color: Colors.red, fontSize: 20),
+                                ),
+                              ],
+                            ),
+                            Center(
+                              child: Text(
+                                "Qestion 3/10",
+                                style: TextStyle(
+                                  color: Color(0xffA42FC1),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 25,
+                            ),
+                            Text('What is computer?')
+                          ],
                         ),
                       ),
-                    )
-                  ],
+                    ),
+                  ),
+                  const Positioned(
+                      bottom: 210,
+                      left: 140,
+                      child: CircleAvatar(
+                        radius: 42,
+                        backgroundColor: Colors.white,
+                        child: Center(
+                          child: Text(
+                            '15',
+                            style: TextStyle(
+                              color: Color(0xffA42FC1),
+                              fontSize: 25,
+                            ),
+                          ),
+                        ),
+                      ))
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xffA42FC1),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  elevation: 5,
                 ),
-              )
-            ],
-          )),
+                onPressed: () {},
+                child: Container(
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'Next',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
