a7896efdd92492871c74d694b0e09330c69dc1ba
aldoprogrammer <aldobesma@gmail.com>
2024-09-15 20:09:32 +0700

    layout and content

--- lib/completed.dart
+++ lib/completed.dart
@@ -10,17 +10,18 @@ class Completed extends StatelessWidget {
         children: [
           SizedBox(
             height: 521,
-            width: 400,
-            child: Stack(children: [
-              Container(
-                height: 340,
-                width: 410,
-                decoration: BoxDecoration(
-                  color: const Color(0xffA42FC1),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Center(
-                  child: CircleAvatar(
+            width: double.infinity, // Set width to 100%
+            child: Stack(
+              children: [
+                Container(
+                  height: 340,
+                  width: double.infinity, // Set width to 100%
+                  decoration: BoxDecoration(
+                    color: const Color(0xffA42FC1),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Center(
+                    child: CircleAvatar(
                       radius: 85,
                       backgroundColor: Colors.white.withOpacity(.3),
                       child: CircleAvatar(
@@ -40,29 +41,195 @@ class Completed extends StatelessWidget {
                               ),
                               RichText(
                                 text: const TextSpan(
-                                    text: '100',
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color(0xffA42FC1),
-                                    ),
-                                    children: [
-                                      TextSpan(
-                                        text: ' pt',
-                                        style: TextStyle(
-                                            fontSize: 15,
-                                            color: Color(0xffA42FC1)),
-                                      )
-                                    ]),
-                              )
+                                  text: '100',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xffA42FC1),
+                                  ),
+                                  children: [
+                                    TextSpan(
+                                      text: ' pt',
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        color: Color(0xffA42FC1),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
                             ],
                           ),
                         ),
-                      )),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ]),
-          )
+                Positioned(
+                  bottom: 60,
+                  left: 0, // Remove specific left offset
+                  right: 0, // Ensure it's centered horizontally
+                  child: Center(
+                    child: Container(
+                      height: 190,
+                      width: MediaQuery.of(context).size.width *
+                          0.9, // 90% of screen width
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Color(0xffA42FC1),
+                            blurRadius: 20,
+                            offset: Offset(0, 10),
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 18),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Container(
+                                            height: 15,
+                                            width: 15,
+                                            decoration: const BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              color: Color(0xffA42FC1),
+                                            ),
+                                          ),
+                                          const Text(
+                                            '100%',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: 20,
+                                              color: Color(0xffA42FC1),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      const Text('Completions')
+                                    ],
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Container(
+                                            height: 15,
+                                            width: 15,
+                                            decoration: const BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              color: Color(0xffA42FC1),
+                                            ),
+                                          ),
+                                          const Text(
+                                            '10',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w500,
+                                              fontSize: 20,
+                                              color: Color(0xffA42FC1),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      const Text('Total Questions')
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 25,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Container(
+                                            height: 15,
+                                            width: 15,
+                                            decoration: const BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              color: Colors.green,
+                                            ),
+                                          ),
+                                          const Padding(
+                                            padding: EdgeInsets.only(left: 5.0),
+                                            child: Text(
+                                              '07',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.w500,
+                                                fontSize: 20,
+                                                color: Colors.green,
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      const Text('Correct Answers')
+                                    ],
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Container(
+                                            height: 15,
+                                            width: 15,
+                                            decoration: const BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              color: Colors.red,
+                                            ),
+                                          ),
+                                          const Padding(
+                                            padding: EdgeInsets.only(left: 5.0),
+                                            child: Text(
+                                              '03',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.w500,
+                                                fontSize: 20,
+                                                color: Colors.red,
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      const Text('Wrong Answers')
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
